fc3b4a3ba57660fbd1bec2b06d52455ede824097
John <fehknuhs@web.de>
2020-10-20 16:26:03 +0200

    HTML input element had no associated label

--- views/LoginView.php
+++ views/LoginView.php
@@ -12,7 +12,7 @@
         <h2 class="text-center">Login</h2>
         <?= /** @var STRING $csrfInputField */ $csrfInputField ?>
         <div class="form-group">
-            <label for="email">Username:</label>
+            <label for="username">Username:</label>
             <input class="form-control" id="username" name="username" placeholder="Username">
         </div>
         <div class="form-group">
